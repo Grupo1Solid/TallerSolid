51fa0bff1fc30f1d9fdf6bdb0e42c3b6934a3317
eduatigs <eduatigs96@gmail.com>
2018-11-08 16:18:37 -0500

    Correccion en la clase CambioLeche del paquete Procesos

---
+++ TallerSOLID/src/ProcesosOCP/CambioLecheOCP.java
@@ -0,0 +1,14 @@
+package ProcesosOCP;
+
+import LecheOCP.Leche;
+
+/**
+ *
+ * @author Acer
+ */
+public class CambioLecheOCP {
+    
+    public void cambiarTipoLeche(Leche tipo){
+        tipo.usar();
+    }
+}
